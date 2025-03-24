a80c548093a05a58057c7d9e3d65eeeb79143480
madondo-tech <phiwayinkosimadondo996@gmail.com>
2025-03-24 13:34:07 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,16 +1,14 @@
 from extensions import db
 from flask_login import UserMixin
 
-# User model
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
     name = db.Column(db.String(100), nullable=True)
     phone = db.Column(db.String(20), nullable=True)
-    role = db.Column(db.String(50), default='student')  # 'student', 'staff', 'admin'
+    role = db.Column(db.String(50), default='student')  
 
-# Incident model
 class Incident(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
